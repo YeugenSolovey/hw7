9f890d90d78990f5f0d5ea7fb98f0705a7ddba88
YeugenSolovey <116490331+YeugenSolovey@users.noreply.github.com>
2022-11-30 08:44:05 +0200

    Add files via upload

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,219 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    //дає змогу створювати сімейства пов’язаних об’єктів, не прив’язуючись до конкретних класів створюваних об’єктів.
+    // AbstractProductA
+    public abstract class Car
+    {
+        public abstract void Info();
+        public void Interact(Engine engine, Color color)
+        {
+            Info();
+            Console.WriteLine("Set Engine: ");
+            engine.GetPower();
+            Console.WriteLine("Set color: ");
+            color.GetColor();
+        }
+    }
+
+    // ConcreteProductA1
+    public class Ford : Car
+    {
+        public override void Info()
+        {
+            Console.WriteLine("Ford");
+        }
+    }
+
+    //ConcreteProductA2
+    public class Toyota : Car
+    {
+        public override void Info()
+        {
+            Console.WriteLine("Toyota");
+        }
+    }
+    //ConcreteProductA3
+    public class Mersedes : Car
+    {
+        public override void Info()
+        {
+            Console.WriteLine("Mersedes");
+        }
+    }
+
+    // AbstractProductB
+    public abstract class Engine
+    {
+        public virtual void GetPower()
+        {
+        }
+    }
+
+    // ConcreteProductB1
+    public class FordEngine : Engine
+    {
+        public override void GetPower()
+        {
+            Console.WriteLine("Ford Engine 4.4");
+        }
+    }
+
+    //ConcreteProductB2
+    public class ToyotaEngine : Engine
+    {
+        public override void GetPower()
+        {
+            Console.WriteLine("Toyota Engine 3.2");
+        }
+    }
+    //ConcreteProductB3
+    public class MersedesEngine : Engine
+    {
+        public override void GetPower()
+        {
+            Console.WriteLine("Mesedes Engine 3.6");
+        }
+    }
+
+    // AbstractProductC
+    public abstract class Color
+    {
+        public virtual void GetColor()
+        {
+        }
+    }
+    public class FordColor : Color
+    {
+        public override void GetColor()
+        {
+            Console.WriteLine("Ford color: red");
+        }
+    }
+
+    //ConcreteProductB2
+    public class ToyotaColor : Color
+    {
+        public override void GetColor()
+        {
+            Console.WriteLine("Toyota color: green");
+        }
+    }
+    //ConcreteProductB3
+    public class MersedesColor : Color
+    {
+        public override void GetColor()
+        {
+            Console.WriteLine("Mesedes color: gray");
+        }
+    }
+
+    // AbstractFactory
+    public interface ICarFactory
+    {
+        Car CreateCar();
+        Engine CreateEngine();
+        Color CreateColor();
+    }
+
+    // ConcreteFactory1
+    public class FordFactory : ICarFactory
+    {
+        // from CarFactory
+        Car ICarFactory.CreateCar()
+        {
+            return new Ford();
+        }
+
+        Engine ICarFactory.CreateEngine()
+        {
+            return new FordEngine();
+        }
+        Color ICarFactory.CreateColor()
+        {
+            return new FordColor();
+        }
+    }
+
+    // ConcreteFactory2
+    public class ToyotaFactory : ICarFactory
+    {
+        // from CarFactory
+        Car ICarFactory.CreateCar()
+        {
+            return new Toyota();
+        }
+
+        Engine ICarFactory.CreateEngine()
+        {
+            return new ToyotaEngine();
+        }
+        Color ICarFactory.CreateColor()
+        {
+            return new ToyotaColor();
+        }
+    }
+
+    // ConcreteFactory3
+    public class MesedesFactory : ICarFactory
+    {
+        // from CarFactory
+        Car ICarFactory.CreateCar()
+        {
+            return new Mersedes();
+        }
+
+        Engine ICarFactory.CreateEngine()
+        {
+            return new MersedesEngine();
+        }
+        Color ICarFactory.CreateColor()
+        {
+            return new MersedesColor();
+        }
+    }
+
+    public class ClientFactory
+    {
+        private Car car;
+        private Engine engine;
+        private Color color;
+
+        public ClientFactory(ICarFactory factory)
+        {
+            //Абстрагування процесів інстанціювання
+            car = factory.CreateCar();
+            engine = factory.CreateEngine();
+            color =  factory.CreateColor();
+        }
+
+        public void Run()
+        {
+            car.GetType();
+            //Абстрагування варіантів використання
+            car.Interact(engine, color);
+        }
+    }
+
+    class AbstractFactoryApp
+    {
+        static void Main(string[] args)
+        {
+            ICarFactory carFactory = new ToyotaFactory();
+            ClientFactory client1 = new ClientFactory(carFactory);
+
+            client1.Run();
+
+            carFactory = new FordFactory();
+            ClientFactory client2 = new ClientFactory(carFactory);
+            client2.Run();
+
+            carFactory = new MesedesFactory();
+            ClientFactory client3 = new ClientFactory(carFactory);
+            client3.Run();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Builder/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//дає змогу створювати складні об’єкти крок за кроком.
+//Будівельник дає можливість використовувати один і той самий код
+//будівництва для отримання різних відображень об’єктів.
+namespace Builder
+{
+    class Program
+    {
+        class Pizza
+        {
+            string dough;
+            string sauce;
+            string topping;
+            string toppingSecond;
+            public Pizza() { }
+            public void SetDough(string d) { dough = d; }
+            public void SetSauce(string s) { sauce = s; }
+            public void SetTopping(string t) { topping = t; }
+
+            public void SetToppingSecond(string ts) { toppingSecond = ts; }
+            public void Info()
+            {
+                Console.WriteLine("Dough: {0}\nSause: {1}\nTopping: {2}\nToping2: {3}", dough, sauce, topping, toppingSecond);
+            }
+        }
+
+        //Abstract Builder
+        abstract class PizzaBuilder
+        {
+            protected Pizza pizza;
+            public PizzaBuilder() { }
+            public Pizza GetPizza() { return pizza; }
+            public void CreateNewPizza() { pizza = new Pizza(); }
+
+            public abstract void BuildDough();
+            public abstract void BuildSauce();
+            public abstract void BuildTopping();
+
+            public abstract void BuildToppingSecond();
+        }
+        //Concrete Builder
+        class HawaiianPizzaBuilder : PizzaBuilder
+        {
+            public override void BuildDough() { pizza.SetDough("cross"); }
+            public override void BuildSauce() { pizza.SetSauce("mild"); }
+            public override void BuildTopping() { pizza.SetTopping("ham+pineapple"); }
+
+            public override void BuildToppingSecond() { pizza.SetToppingSecond("chees"); }
+        }
+        //Concrete Builder
+        class SpicyPizzaBuilder : PizzaBuilder
+        {
+            public override void BuildDough() { pizza.SetDough("pan baked"); }
+            public override void BuildSauce() { pizza.SetSauce("hot"); }
+            public override void BuildTopping() { pizza.SetTopping("pepparoni+salami"); }
+
+            public override void BuildToppingSecond() { pizza.SetToppingSecond("chees"); }
+        }
+        class MargaritaPizzaBuilder : PizzaBuilder
+        {
+            public override void BuildDough() { pizza.SetDough("pan baked"); }
+            public override void BuildSauce() { pizza.SetSauce("hot"); }
+            public override void BuildTopping() { pizza.SetTopping("pepparoni+tomat"); }
+
+            public override void BuildToppingSecond() { pizza.SetToppingSecond("chees"); }
+        }
+        /** "Director" */
+        class Waiter
+        {
+            private PizzaBuilder pizzaBuilder;
+            public void SetPizzaBuilder(PizzaBuilder pb) { pizzaBuilder = pb; }
+            public Pizza GetPizza() { return pizzaBuilder.GetPizza(); }
+            public void ConstructPizza()
+            {
+                pizzaBuilder.CreateNewPizza();
+                pizzaBuilder.BuildDough();
+                pizzaBuilder.BuildSauce();
+                pizzaBuilder.BuildTopping();
+                pizzaBuilder.BuildToppingSecond();
+            }
+        }
+        /** A customer ordering a pizza. */
+        class BuilderExample
+        {
+            public static void Main(String[] args)
+            {
+                Waiter waiter = new Waiter();
+                PizzaBuilder hawaiianPizzaBuilder = new HawaiianPizzaBuilder();
+                PizzaBuilder spicyPizzaBuilder = new SpicyPizzaBuilder();
+                PizzaBuilder margaritaPizzaBuilder = new MargaritaPizzaBuilder();
+
+                waiter.SetPizzaBuilder(margaritaPizzaBuilder);
+                waiter.ConstructPizza();
+
+                Pizza pizza = waiter.GetPizza();
+                pizza.Info();
+
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
